de492f8e688f995562fe708fd6cd990215466590
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-04-23 18:11:43 -0300

    Criação de componente para cabeçalho
    Criação de 'header', com a instalação das dependências utilizadas por ele

---
+++ src/components/Header.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { SafeAreaView, View, Text, Image, StyleSheet, Dimensions } from 'react-native'
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+
+import userImg from '../assets/maby.png'
+import colors from '../styles/colors'
+import fonts from '../styles/fonts'
+
+export function Header(){
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Olá,</Text>
+        <Text style={styles.userName}>Maby</Text>
+      </View>
+      <Image source={userImg} style={styles.image}></Image>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    marginTop: getStatusBarHeight(),
+    padding: 30
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text
+  },
+  userName: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 40
+  },
+  image: {
+    width: 80,
+    height: 80,
+    borderRadius: 40
+  }
+})
